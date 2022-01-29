153751ab1f9f5d53bfdfc2efba5833eeecdc48a6
Oliver Brandt <oliver.brandt@hamburg.de>
2022-01-29 18:38:31 +0100

    style: Setup ESLint and apply Google style

--- src/BooksAPI.js
+++ src/BooksAPI.js
@@ -1,44 +1,48 @@
+const api = 'https://reactnd-books-api.udacity.com';
 
-const api = "https://reactnd-books-api.udacity.com"
-
-
-// Generate a unique token for storing your bookshelf data on the backend server.
-let token = localStorage.token
-if (!token)
-  token = localStorage.token = Math.random().toString(36).substr(-8)
-
-const headers = {
-  'Accept': 'application/json',
-  'Authorization': token
+// Generate a unique token for storing your bookshelf data on the backend server
+let token = localStorage.token;
+if (!token) {
+  token = localStorage.token = Math.random()
+      .toString(36)
+      .substring(-8);
 }
 
+const headers = {
+  Accept: 'application/json',
+  Authorization: token,
+};
+
 export const get = (bookId) =>
-  fetch(`${api}/books/${bookId}`, { headers })
-    .then(res => res.json())
-    .then(data => data.book)
+  fetch(`${api}/books/${bookId}`, {headers})
+      .then((res) => res.json())
+      .then((data) => data.book);
 
 export const getAll = () =>
-  fetch(`${api}/books`, { headers })
-    .then(res => res.json())
-    .then(data => data.books)
+  fetch(`${api}/books`, {headers})
+      .then((res) => res.json())
+      .then((data) => data.books);
 
 export const update = (book, shelf) =>
   fetch(`${api}/books/${book.id}`, {
     method: 'PUT',
     headers: {
       ...headers,
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ shelf })
-  }).then(res => res.json())
+    body: JSON.stringify({shelf}),
+  }).then((res) => res.json());
 
+// Searches by author and title, but only for a specific set of terms
+// documented here: SEARCH_TERMS.md
 export const search = (query) =>
   fetch(`${api}/search`, {
     method: 'POST',
     headers: {
       ...headers,
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ query })
-  }).then(res => res.json())
-    .then(data => data.books)
+    body: JSON.stringify({query}),
+  })
+      .then((res) => res.json())
+      .then((data) => data.books);
